b971e6d93b24e5f69e2165fc9fb4b7f0a48ddbef
Cam205 <cameronbuchanan17@yahoo.com>
2021-10-12 18:13:22 -0500

    Fixed some small things with await/async

--- promising-results-master/app.js
+++ promising-results-master/app.js
@@ -1,4 +1,4 @@
-slowMath.add(6, 2).then(num => {
+/* slowMath.add(6, 2).then(num => {
     console.log(num);
     return slowMath.multiply(num, 2);
 }).then(num => {
@@ -27,3 +27,37 @@ slowMath.add(6, 2).then(num => {
 }).catch(err => {
     console.log(err);
 });
+*/
+
+let doMath = async () => {
+    try {
+
+        let result;
+
+        result = await slowMath.add(6, 2)
+        console.log(result)
+        result = await slowMath.multiply(result, 2)
+        console.log(result)
+        result = await slowMath.divide(result, 4)
+        console.log(result)
+        result = await slowMath.subtract(result, 3)
+        console.log(result)
+        result = await slowMath.add(result, 98)
+        console.log(result)
+        result = await slowMath.remainder(result, 2)
+        console.log(result)
+        result = await slowMath.multiply(result, 50)
+        console.log(result)
+        result = await slowMath.remainder(result, 40)
+        console.log(result)
+        result = await slowMath.add(result, 32)
+        console.log(result)
+    }
+    catch (err) {
+        throw (err)
+
+        console.log()
+    }
+}
+
+doMath();
